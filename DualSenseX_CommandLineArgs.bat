f621f20b854f74dcdd6b6a1ff3e35c0a089379af
Paliverse <mohammed_sharawi@yahoo.com>
2021-09-16 05:12:02 -0500

    Add files via upload

--- DualSenseX_CommandLineArgs.bat
+++ DualSenseX_CommandLineArgs.bat
@@ -1,12 +1,30 @@
 @ECHO OFF
-cd %USERPROFILE%\AppData\Local\DualSenseX
-START /B DualSenseX.exe --GameName "GAMENAME" --FileLocation "%~dp0TEXTFILENAME.txt"
+
+:: HOW TO USE:
+:: - Replace GAMENAME with your game name.
+:: - Replace TEXTFILENAME.txt with your text file name, while also including .txt extension and the quote after it.
+:: - BatchFileLocation will get the directory of where this batch file is located on user's PC.
+:: - Make sure the batch file and the text file are located in the same folder directory
+
+
+
+:: Set your game name after the Equals:
+set GameName=GAMENAME
+
+:: Set your text file name after the Equals while keeping the quote after it:
+set "TextFileName=TEXTFILENAME.txt"
+
+
+
+
+
+set /p AppLocation=< C:\Temp\DualSenseX\DualSenseXLocation.txt
+
+set "BatchFileLocation=%~dp0"
+
+set "FinalTextLocation=%BatchFileLocation%%TextFileName%"
+
+cd /D %AppLocation%
+
+START /B DualSenseX.exe --GameName %GameName% --FileLocation %FinalTextLocation%
 exit
-
-
-Info and how to use:
-
-- @ECHO OFF and /B will stop the command prompt from opening a window when executing this batch file.
-- %~dp0 will get the directory of where this batch file is located on user's PC.
-- Replace GAMENAME with your game name.
-- Replace TEXTFILENAME.txt with your text file name, while also including .txt extension. DONT put a space after the 0!
